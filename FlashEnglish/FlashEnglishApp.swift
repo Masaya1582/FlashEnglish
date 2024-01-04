a2f840e73dbe49e5dd2efde0a2d200f183cf7d3b
Masaya1582 <masa8y88@gmail.com>
2024-01-04 14:37:45 +0900

    Initial Commit

---
+++ FlashEnglish/FlashEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashEnglishApp.swift
+//  FlashEnglish
+//
+//  Created by MasayaNakakuki on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct FlashEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
