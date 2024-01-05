8d862d2df9309a36ab0a9abb8267634e1d008238
Masaya Nakakuki <masa8y88@gmail.com>
2024-01-05 15:17:49 +0900

    Remake Model and ViewModel

---
+++ FlashEnglish/Sources/Models/HomeModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeModel.swift
+//  FlashEnglish
+//
+//  Created by 中久木 雅哉(Nakakuki Masaya) on 2024/01/05.
+//  Copyright (c) 2024 ReNKCHANNEL. All rihgts reserved.
+//
+
+import Foundation
